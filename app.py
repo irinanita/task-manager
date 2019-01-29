9383655a1e603ffdac92a3c27d52b92960aff6fe
Irina Nita <irinanita.im@gmail.com>
2019-01-29 13:56:44 +0000

    added accordion

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 import os
-from flask import Flask
+from flask import Flask,render_template,redirect,request,url_for
+from flask_pymongo import PyMongo
 
 app =Flask(__name__)
+app.config["MONGO_DBNAME"] = 'task_manager'
+app.config["MONGO_URI"] ='mongodb://admin:ceckbrb05@ds253284.mlab.com:53284/task_manager'
+
+mongo = PyMongo(app)
+
 
 @app.route('/')
-def hello():
-    return"Hello World...again"
-    
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("tasks.html",
+    tasks=mongo.db.tasks.find(), port = os.environ.get('PORT'))
+
+print(os.environ.get('PORT'))
+
 if __name__ =='__main__':
     app.run(host=os.environ.get('IP'),port=int(os.environ.get('PORT')),
         debug=True)
