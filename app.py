2c155996dbac91059035f866f09ebe9890896831
Irina Nita <irinanita.im@gmail.com>
2019-01-30 15:30:38 +0000

    added edittask form & some styling

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask,render_template,redirect,request,url_for
 from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app =Flask(__name__)
 app.config["MONGO_DBNAME"] = 'task_manager'
@@ -21,11 +22,19 @@ def add_task():
     category_list = [category for category in _categories]
     return render_template('addtask.html',
             categories=category_list)
+
 @app.route('/insert_task',methods=['POST'])
 def insert_task():
     tasks = mongo.db.tasks
     tasks.insert_one(request.form.to_dict())
     return redirect(url_for('get_tasks'))
+    
+@app.route('/edit_task/<task_id>')
+def edit_task(task_id):
+    the_task = mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
+    all_categories = mongo.db.categories.find()
+    return render_template('edittask.html',task=the_task,categories=all_categories)
+    
 
 if __name__ =='__main__':
     app.run(host=os.environ.get('IP'),port=int(os.environ.get('PORT')),
